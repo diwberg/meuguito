e8de2521eab620cb8267abcc0ff5a1f5536c28fa
Diwberg <diwberg@gmail.com>
2023-06-13 13:36:10 +0100

    Final version release v1.0.1

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -24,7 +24,7 @@ export function NewTransactionModal() {
     control,
     register,
     handleSubmit,
-    formState: { isSubmitting, isSubmitSuccessful },
+    formState: { isSubmitting },
     reset,
   } = useForm<NewTransactionFormInput>({
     resolver: zodResolver(newTransactionFormSchema),
